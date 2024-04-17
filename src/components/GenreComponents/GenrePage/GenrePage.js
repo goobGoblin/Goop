d348cfed3ef7cb49c8af7cfa37367363a660d934
Chinh Nguyen <nguyenchuu@yahoo.com>
2024-04-17 02:09:46 -0500

    Connected to the backend and displays "Genres" table with "Name" and "Description" column

--- src/components/GenreComponents/GenrePage/GenrePage.js
+++ src/components/GenreComponents/GenrePage/GenrePage.js
@@ -6,22 +6,42 @@ function GenrePage() {
   const [genres, setGenres] = useState([]);
 
   useEffect(() => {
-    // Dynamically import all JSON files from a specific folder
-    function importAll(r) {
-      return r.keys().map(r);
-    }
+    fetch('/api/genres')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Fetched genres:', data);  // Log the fetched data to see what it looks like
+        setGenres(data);
+      })
+      .catch(error => {
+        console.error('Error fetching genres:', error);
+      });
 
-    const genreData = importAll(require.context('../../../../data_handling/raw_data/Genres-JSON', false, /\.json$/));
-    setGenres(genreData);
+    // Dynamically import all JSON files from a specific folder
+    // function importAll(r) {
+    //   return r.keys().map(r);
+    // }
+
+    // const genreData = importAll(require.context('../../../../data_handling/raw_data/Genres-JSON', false, /\.json$/));
+    // setGenres(genreData);
   }, []);
+  
 
   return (
     <div className="genre-page-container">
       <h1>Genres</h1>
       <p className="description">Clicking on a genre will reveal several sub-genres.</p>
+
       {genres.map((genre, index) => (
-        <Genre key={index} genre={genre} />
+        <div key={index}>
+          <h2>{genre.Name}</h2>
+          <p>{genre.Description}</p>
+        </div>
       ))}
+
+      {/* I commented this out because I didn't want to mess with other files (namely Genre.js), and wanted to get something working. */}
+      {/* {genres.map((genre, index) => (
+        <Genre key={index} genre={genre} />
+      ))} */}
     </div>
   );
 }
